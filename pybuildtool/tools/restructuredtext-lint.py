84429a0f892e29838e20f44388bebe70b94f2752
Fahri Reza <dozymoe@gmail.com>
2017-02-14 15:03:10 +0700

    refactor, restructuredtext-lint allow ignores

--- pybuildtool/tools/restructuredtext-lint.py
+++ pybuildtool/tools/restructuredtext-lint.py
@@ -3,8 +3,12 @@ Lint reStructuredText files.
 
 Options:
 
-    * format : str, None, report format (text, json)
-    * encoding: str, None, file encoding (utf-8)
+    * encoding         : str, 'utf-8'
+                         File encoding, argument to python open()
+    * ignore_directives: list, []
+                         Ignore errors about unknown directives.
+    * ignore_roles     : list, []
+                         Ignore errors about unknown roles.
 
 Requirements:
 
@@ -15,7 +19,10 @@ Requirements:
 
 """
 
+import os
+import re
 from pybuildtool.core.task import Task as BaseTask
+from pybuildtool.misc.collections import make_list
 
 tool_name = __name__
 
@@ -23,30 +30,76 @@ class Task(BaseTask):
 
     name = tool_name
 
+    encoding = None
+    regex_ignores = None
+
     def prepare(self):
         cfg = self.conf
-        args = self.args
-
-        # Format of the output (e.g. text, json)
-        c = cfg.get('format', None)
-        if c:
-            args.append('--format=' % c)
 
         # Encoding of the input file (e.g. utf-8)
-        c = cfg.get('encoding', None)
-        if c:
-            args.append('--encoding=' % c)
+        self.encoding = cfg.get('encoding', 'utf-8')
+
+        self.regex_ignores = []
+
+        # Ignore unknown directives
+        unknown = make_list(cfg.get('ignore_directives'))
+        if len(unknown):
+            self.regex_ignores.append(re.compile('^Unknown directive type "(' +\
+                    '|'.join(unknown) + ')".*'))
+
+            self.regex_ignores.append(re.compile('^No directive entry for "(' +\
+                    '|'.join(unknown) + ')".*'))
+
+        # Ignore unknown roles
+        unknown = make_list(cfg.get('ignore_roles'))
+        if len(unknown):
+            self.regex_ignores.append(re.compile('^Unknown interpreted text ' +\
+                    'role "(' + '|'.join(unknown) + ')".*'))
+
+            self.regex_ignores.append(re.compile('^No role entry for "(' +\
+                    '|'.join(unknown) + ')".*'))
 
 
     def perform(self):
-        executable = self.env['%s_BIN' % tool_name.upper()]
-        return self.exec_command(
-            '{exe} {arg} {in_}'.format(
-            exe=executable,
-            arg=' '.join(self.args),
-            in_=' '.join(self.file_in),
-        ))
+        from restructuredtext_lint import lint_file
+
+        result = 0
+        for filename in self.file_in:
+            errors = lint_file(filename, encoding=self.encoding)
+            if len(errors) == 0:
+                continue
+
+            relpath = os.path.relpath(filename)
+            if relpath.startswith('.'):
+                relpath = filename
+
+            print_filename = True
+            for error in errors:
+                ignored = False
+                for re_ignore in self.regex_ignores:
+                    if re_ignore.match(error.message):
+                        ignored = True
+                        break
+
+                if ignored:
+                    continue
+
+                if print_filename:
+                    print('************* File ' + relpath)
+                    print_filename = False
+
+                if error.level > 2:
+                    result += 1
+
+                message = error.message.replace('\n', ' ')
+                print('%s: %i: %s' % (error.type[0], error.line, message))
+
+        return result
 
 
 def configure(conf):
-    conf.env['%s_BIN' % tool_name.upper()] = conf.find_program(tool_name)[0]
+    conf.start_msg("Checking for python module '%s'" % tool_name)
+    try:
+        import restructuredtext_lint # pylint: disable=unused-import
+    except ImportError:
+        conf.end_msg('not found', color='YELLOW')
